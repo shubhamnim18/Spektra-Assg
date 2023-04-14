d6ebcc6cea3cc903b7d0c0f90726df21bcdb0e7e
shubhamnim18 <shubhamnimbalkar9618@gmail.com>
2023-04-14 17:52:03 +0530

    Assignments

---
+++ Test 13-04-2023/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MobileService } from '../mobile.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  mobiles: { image: string, name: string, price: number, color: string, spec: string, camera: string, ram: string, battery: string, quantity:number }[] = [];
+
+  constructor(private mobile: MobileService) {
+
+  }
+
+  ngOnInit(): void {
+    this.mobiles = this.mobile.getMobiles();
+  }
+
+  setDetails(mob: { image: string, name: string, price: number, color: string, spec: string, camera: string, ram: string, battery: string, quantity:number }) {
+    this.mobile.setDetails(mob);
+  }
+}
---
+++ Test 13-04-2023/src/app/mobile.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobileService {
+
+  mobiles=[
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-max-1.jpg',name:'Iphone 14 Pro Max',price:149999,color:'Grey',spec:'Released 2022, September 16 240g, 7.9mm thickness iOS 16, up to iOS 16.4.1 128GB/256GB/1TB storage, no card slot',camera:'16MP',ram:'6Gb',battery:'3240mAh',quantity: 1},
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-3.jpg',name:'Iphone 14 Pro',price:129999,color:'Black',spec:'Released 2022, September 16 206g, 7.9mm thickness iOS 16, up to iOS 16.4.1 128GB/256GB/1TB storage, no card slot',camera:'14MP',ram:'4Gb',battery:'3240mAh',quantity: 1},
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-3.jpg',name:'Iphone 14',price:109999,color:'Blue',spec:'Released 2022, September 16 172g, 7.8mm thickness iOS 16, up to iOS 16.4.1 128GB/256GB/512GB storage, no card slot',camera:'12MP',ram:'4Gb',battery:'3240mAh',quantity: 1},
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-pro-max-01.jpg',name:'Iphone 13 Pro Max',price:129999,color:'Grey',spec:'Released 2021, September 24 240g, 7.7mm thickness iOS 15, up to iOS 16.4.1 128GB/256GB/1TB storage, no card slot',camera:'14MP',ram:'6Gb',battery:'3240mAh',quantity: 1},
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-pro-01.jpg',name:'Iphone 13 Pro',price:99999,color:'Grey',spec:'Released 2021, September 24 204g, 7.7mm thickness iOS 15, up to iOS 16.4.1 128GB/256GB/1TB storage, no card slot',camera:'12MP',ram:'4Gb',battery:'3240mAh',quantity: 1},
+    {image:'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg',name:'Iphone 13',price:79999,color:'Grey',spec:'Released 2021, September 24 174g, 7.7mm thickness iOS 15, up to iOS 16.4.1 128GB/256GB/512GB storage, no card slot',camera:'12MP',ram:'4Gb',battery:'3240mAh',quantity: 1},
+  ]
+
+  mobile:{ image: string, name: string, price: number, color: string, spec: string, camera: string, ram:string, battery:string, quantity:number }[]=[];
+  constructor() { }
+
+  getMobiles(){
+    return this.mobiles;
+  }
+
+  setDetails(mob:{ image: string, name: string, price: number ,color: string, spec: string, camera: string, ram:string, battery:string, quantity:number  }){
+    this.mobile.push(mob);
+  }
+
+  getDetails(){
+    return this.mobile;
+  }
+
+}
